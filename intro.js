31bb902151d38a2240d32875ccd23a4fc50cd20e
Jeff Church <jeffmichaelchurch@gmail.com>
2017-05-17 19:04:38 -0500

    had files saved in html instead of .js, corrected now

--- intro.js
+++ intro.js
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <script>
-                console.log("Introduction to JavaScript");
-            </script>
-            
-            
 
-        </head>
-    </html>
+                console.log("Introduction to JavaScript");
+            
+            
+                function fizzBuzz(){
+                var i;
+            for( i = 1; i < 101; i++){
+                if(i % 5 === 0 && i % 3 === 0){
+                console.log("fizzbuzz");
+                 }else if(i % 5 === 0 && i % 3 !== 0){
+                    console.log("buzz");
+                 }else if(i % 3 === 0){
+                    console.log("fizz");
+                }else{
+                console.log(i);
+            }
+            }
+                }
+                    fizzBuzz();
+                function countBs(wordToCount){
+                    var counter = countChar(wordToCount, "B");
+                    return counter;
+                }
+                countBs("Bobs Brown Burgers");
+                function countChar(wordToCount, charToCount){
+                    var count = 0;
+                    var looper;
+                    for(looper = 0; looper < wordToCount.length; looper++){
+                        var letter = wordToCount.charAt(looper);
+                        if(letter === charToCount){
+                            count++;
+                        }
+                    }
+                    return count;
+                }
+                countChar("I love everything.", "e");
